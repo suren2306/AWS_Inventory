05dcd8e009a8dbc3bc575c2aa5aefb66dfb05297
Surendar <surendar.m@gmail.com>
2019-04-02 13:35:38 +0530

    Added master script that executes other scripts and combine the files to a single xls file

---
+++ CSV/Report.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+date=`date +%Y%m%d`
+echo "Running Ec2 Script"
+./all_instances_inCSV.sh > EC2
+echo "Running Snapshots Script"
+./all_snapshots_inCSV.sh > Snapshots
+echo "Running EBS script"
+./all_ebs_inCSV.sh > EBS
+echo "Running AMI Script"
+./all_ami_inCSV.sh > AMI
+echo "Running Users Script"
+./all_aws_user_with_policy_and_MFA_inCSV.sh > Users
+#ssconvert package needs to be installed already
+ssconvert --merge-to=Report_$date.xls EC2 EBS Users AMI Snapshots
+rm EC2 Users AMI Snapshots EBS

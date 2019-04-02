d3ae3d9bbe2f79d1cf3b9f7bdff1bd119ca52e53
Surendar <surendar.m@gmail.com>
2019-04-02 12:33:12 +0530

    Removed duplicated scripts

--- CSV/ebs_report_all_regions.sh
+++
@@ -1,8 +0,0 @@
-for region in `aws ec2 describe-regions --output text | cut -f3`
-do
-    result_fname=$region'_ebs.csv'
-    python ebs-report.py --regions $region --file $result_fname
-done
-head -1 $result_fname > ebs_final.csv
-for filename in $(ls *ebs.csv); do sed 1d $filename >> ebs_final.csv ; done
-rm *ebs.csv

ad4fec6fb48ebe5c61d1657d9c89412f6502927f
Surendar <surendar.m@gmail.com>
2019-04-03 11:41:26 +0530

    Fixed wrong headings

--- CSV/all_ami_inCSV.sh
+++ CSV/all_ami_inCSV.sh
@@ -4,14 +4,14 @@
 declare -a AllProfiles
 #echo "Gathering profiles..."
 AllProfiles=( $(./AllProfiles.sh ProfileNameOnly | awk '{print $1}') )
-format='%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'
+format='%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s\n'
 #echo "Outputting all EC2 instances from all profiles"
-printf "$format" "Profile" "Region" "AMI Name" "AMI ID" "Description" "Disk 1" "Disk 2" "Disk 3" "Disk 4" "Disk 5"
+printf "$format" "Profile" "Region" "AMI Name" "AMI ID" "Created On" "Disk 1" "Disk 2" "Disk 3" "Disk 4" "Disk 5" "Description"
 for profile in ${AllProfiles[@]}; do
     #Enter a vaild aws profile in the below line
     for regions in `aws ec2 describe-regions --region ap-south-1 --profile $profile --output text | cut -f3`; do
     #echo -e Checking $regions for $profile
-    aws ec2 describe-images --owner self --output text --query 'Images[].[Tags[?Key==`Name`]|[0].Value,CreationDate,ImageId,BlockDeviceMappings[]|[0].Ebs.VolumeSize,BlockDeviceMappings[]|[1].Ebs.VolumeSize,BlockDeviceMappings[]|[2].Ebs.VolumeSize,BlockDeviceMappings[]|[3].Ebs.VolumeSize,BlockDeviceMappings[]|[4].Ebs.VolumeSize]' --profile $profile --region $regions | awk -F $"\t" -v rgn=${regions} -v var=${profile} -v fmt="${format}" '{printf fmt,var,rgn,$1,$2,$3,$4,$5,$6,$7,$8}'
+    aws ec2 describe-images --owner self --output text --query 'Images[].[Tags[?Key==`Name`]|[0].Value,ImageId,CreationDate,BlockDeviceMappings[]|[0].Ebs.VolumeSize,BlockDeviceMappings[]|[1].Ebs.VolumeSize,BlockDeviceMappings[]|[2].Ebs.VolumeSize,BlockDeviceMappings[]|[3].Ebs.VolumeSize,BlockDeviceMappings[]|[4].Ebs.VolumeSize,Description]' --profile $profile --region $regions | awk -F $"\t" -v rgn=${regions} -v var=${profile} -v fmt="${format}" '{printf fmt,var,rgn,$1,$2,$3,$4,$5,$6,$7,$8,$9}'
     done
 done
 echo

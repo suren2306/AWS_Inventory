2835e5ea3d1ff9835e59bd137a7e2509bdeaed44
Surendar M <surendar.m@whatarage.com>
2020-02-03 11:09:45 +0530

    Added loop validation statement

--- CSV/all_instances_inCSV.sh
+++ CSV/all_instances_inCSV.sh
@@ -9,8 +9,13 @@ printf "$format" "Profile" "Region" "Instance Name" "Instance Type" "Instance ID
 #Enter a vaild aws profile in the below line
 for profile in ${AllProfiles[@]}; do
     for regions in `aws ec2 describe-regions --region ap-south-1 --profile $profile --output text | cut -f3`; do
-    #echo -e "\n\nChecking in $regions for $profile account"
+    echo -e "\nChecking EC2 in $regions for $profile account"
     aws ec2 describe-instances --output text --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`]|[0].Value,InstanceType,InstanceId,State.Name,PrivateIpAddress,PublicIpAddress,LaunchTime]' --profile $profile --region $regions | awk -F $"\t" -v rgn=${regions} -v var=${profile} -v fmt="${format}" '{printf fmt,var,rgn,$1,$2,$3,$4,$5,$6,$7}'
+        if [ $? -eq 0 ]; then
+        echo -e "\nListed EC2 in $regions for $profile account succesfully"
+        else
+        echo -e "\nFailed to list EC2 in $regions for $profile account"
+        fi
     done
 done
 echo
